75bc59015a5f694499b558bab6060b7b73c5e498
krish moradiya <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2025-07-22 10:39:36 +0530

    Add files via upload

---
+++ 01basicreact/App.js
@@ -0,0 +1,36 @@
+
+function App() {
+  return (
+    <><h1>Hello Kriskumar..!</h1><p>react
+
+// for creating or install packages of react for react apk.</p><p>
+npx create-react-app 01basicreact
+</p>
+<p>
+// for start execution of apk</p>
+<p>npm run start</p>
+<p>
+vite
+</p><p>
+// create apk via vite.</p><p>
+npm create vite@latest</p><p>
+language javascript </p>
+<p>
+//just once check that all dir are available or not </p><p>
+dir</p><p>
+
+//than go to folder via cd nameOfFolder</p><p>
+
+cd 01vitereact</p><p>
+
+//than install the all packages</p><p>
+
+npm install</p><p>
+
+//for run the project in vite</p><p>
+npm run dev
+</p></>
+  );
+}
+
+export default App;
